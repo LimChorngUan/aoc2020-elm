1043f96360d53d09998fd55cff5b09e501a376ab
Lim Chorng UanE <limchornguan418@gmail.com>
2020-12-08 20:52:15 +0100

    Add parse to parse str to list of string

--- src/Parser.elm
+++ src/Parser.elm
@@ -1,9 +1,19 @@
 module Parser exposing (..)
+
 import Maybe
 
 
 parseListInt : String -> List Int
-parseListInt s = s
-    |> String.split "\n"
-    |> List.map String.trim
-    |> List.filterMap String.toInt
+parseListInt s =
+    s
+        |> String.split "\n"
+        |> List.map String.trim
+        |> List.filterMap String.toInt
+
+
+parseListStr : String -> List String
+parseListStr s =
+    s
+        |> String.split "\n"
+        |> List.map String.trim
+        |> List.filter (not << String.isEmpty)
--- tests/ParserTest.elm
+++ tests/ParserTest.elm
@@ -1,9 +1,9 @@
 module ParserTest exposing (suite)
-import Parser exposing (parseListInt)
+import Parser exposing (parseListInt, parseListStr)
 import Expect exposing (equal)
 import Test exposing (..)
 
-
+intStr : String
 intStr = """
 1
 2
@@ -11,10 +11,21 @@ intStr = """
 4
 """
 
+strStr : String
+strStr = """
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"""
+
+
 suite : Test
 suite =
-    describe "parseListInt"
-        [ test "should transform string to a list of integer" <|
+    describe "Parser"
+        [ test "parseListInt should transform a long string to a list of integer" <|
               \_ -> parseListInt intStr
-                  |> equal [1, 2, 3 , 4]
+                  |> equal [1, 2, 3, 4]
+        , test "parseListStr should transform a long string to a list of string" <|
+              \_ -> parseListStr strStr
+                  |> equal ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
         ]
